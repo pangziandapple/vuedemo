9f5aadcb8854fffbbcea2b2239a27fbe677f48f9
zhangzhiheng <123456>
2018-12-25 16:39:35 +0800

    chusji

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,34 +1,12 @@
 <template>
     <el-container style="height:100%">
         <el-aside>
-          <el-menu
-            default-active="2"
-            class="el-menu-vertical-demo"
-            @open="handleOpen"
-            @close="handleClose">
-            <el-submenu index="1">
-              <template slot="title">
-                <i class="el-icon-location"></i>
-                <span>导航一</span>
-              </template>
-              <el-menu-item-group>
-                <template slot="title">分组一</template>
-                <el-menu-item index="1-1">选项1</el-menu-item>
-                <el-menu-item index="1-2">选项2</el-menu-item>
-              </el-menu-item-group>
-              <el-menu-item-group title="分组2">
-                <el-menu-item index="1-3">选项3</el-menu-item>
-              </el-menu-item-group>
-              <el-submenu index="1-4">
-                <template slot="title">选项4</template>
-                <el-menu-item index="1-4-1">选项1</el-menu-item>
-              </el-submenu>
-            </el-submenu>
+          <el-menu style="height:100%">
             <el-menu-item index="2">
               <i class="el-icon-menu"></i>
               <span slot="title">导航二</span>
             </el-menu-item>
-            <el-menu-item index="3" disabled>
+            <el-menu-item index="3">
               <i class="el-icon-document"></i>
               <span slot="title">导航三</span>
             </el-menu-item>
@@ -39,9 +17,9 @@
           </el-menu>
         </el-aside>
         <el-container>
-          <el-header class="header" height="20px">后台管理系统</el-header>
+          <el-header class="header" height="20px">标题</el-header>
           <el-main>Main</el-main>
-          <el-footer class="footer" >豫ICP备17039344号-2</el-footer>
+          <el-footer class="footer" >备案号</el-footer>
         </el-container>
     </el-container>
 </template>
